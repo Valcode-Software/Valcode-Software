d695529cd87e4b899a39aab860e687be06876fd7
santiagocto <jesusmoyano111@gmail.com>
2026-03-06 16:49:36 -0500

    Correcion de approuter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
 import { motion } from "framer-motion";
 import MainLayout from "../layouts/MainLayout";
 import Hero from "../components/hero/hero";
@@ -7,17 +6,12 @@ import Estadisticas from "../components/estadisticas/Estadisticas";
 import Tecnologias from "../components/tecnologias/Tecnologias";
 import Services from "../components/servicios/services";
 import Aliados from "../components/aliados/strategicallybadge";
+import PorqueValcode from "../components/porque/porque";
 import Nosotros from "../pages/nosotros";
 import ContactPage from "../pages/contacto";
 import Terminos from "../pages/tyc/tyc";
 import Politica from "../pages/politicadeseguirdad/politica";
 
-// Componente de Carga (Loader)
-const Loading = () => (
-  <div className="flex items-center justify-center min-h-screen bg-[#020617]">
-    <div className="w-16 h-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
-  </div>
-);
 
 const ScrollReveal = ({ children }) => {
   return (
@@ -45,6 +39,9 @@ const AppRouter = () => {
                 <Estadisticas />
               </ScrollReveal>
               <ScrollReveal>
+                <PorqueValcode />
+              </ScrollReveal>
+              <ScrollReveal>
                 <Tecnologias />
               </ScrollReveal>
               <Services />
@@ -53,26 +50,10 @@ const AppRouter = () => {
               </ScrollReveal>
             </div>
           } />
-          <Route path="/nosotros" element={
-            <Suspense fallback={<Loading />}>
-              <Nosotros />
-            </Suspense>
-          } />
-          <Route path="/contacto" element={
-            <Suspense fallback={<Loading />}>
-              <ContactPage />
-            </Suspense>
-          } />
-          <Route path="/terminos" element={
-            <Suspense fallback={<Loading />}>
-              <Terminos />
-            </Suspense>
-          } />
-          <Route path="/privacidad" element={
-            <Suspense fallback={<Loading />}>
-              <Politica />
-            </Suspense>
-          } />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<ContactPage />} />
+          <Route path="/terminos" element={<Terminos />} />
+          <Route path="/privacidad" element={<Politica />} />
         </Route>
       </Routes>
     </BrowserRouter>
